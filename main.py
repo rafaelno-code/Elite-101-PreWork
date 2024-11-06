9dff0140606a400aa9cb366b4b7594631dcdd336
rafaelno-code <rafaelnc.calderon@gmail.com>
2024-11-06 00:56:43 -0600

    Person class created and fixed up the information collecting method

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import Person
 def choose_option():
     option = 0
     while(option != 3):
@@ -8,30 +9,31 @@ def choose_option():
                 return option
             except ValueError:
                 print("\nValue error, input an integer between 1 - 3!\n")
-                continue
 
 def info_collection(age):
     if age < 18:
         print("Parent Information Required!")
-        parent_name = input("1. Parent Name: ")
+        parent_name = input("1. Parent Full Name: ")
         parent_age = input("2. Parent Age: ")
         while(True):
             try:
                 parent_id = int(input("3. Parent Social Security(XXXXXXXXX): "))
                 if(len(str(parent_id)) != 9):
                     print("Enter the number in this format(XXXXXXXXX)")
+                    continue
                 break
             except ValueError:
                 print("Value Error, please enter an integer value!")
+        parent_person = Person(parent_name, parent_age, parent_id)
     print("Your information is required:")
-    user_name = input("1. Name: ")
-    user_age = input("2. Age: ")
+    user_name = input("1. Full Name: ")
     while(True):
         try:
-            user_id = int(input("3. Parent Social Security(XXXXXXXXX): "))
+            user_id = int(input("2. Social Security(XXXXXXXXX): "))
             if(len(str(user_id)) != 9):
-                print("Enter the number in this format(XXXXXXXXX)")
-                break
+                print("Enter the 9 digits in this format(XXXXXXXXX)")
+                continue
+            break
         except ValueError:
             print("Value Error, please enter an integer value!")
 
