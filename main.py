13eaf2818370ab807b444f66dee465827e3a97d1
rafaelno-code <rafaelnc.calderon@gmail.com>
2024-11-06 00:07:03 -0600

    indentation changes

--- main.py
+++ main.py
@@ -1,33 +1,31 @@
-class Chatbot:
-
-    print("Hello! I am the Wells Fargo Chatbot!\nHow can I help you today?")
-    option = 0
-    while(option != 5):
-        print("Would you like to...\n1. View Your Accounts\n2. Create An Account\n3. Exit")
-        while(True):
-            try:
-                option = int(input("Choose an option!(Enter a number): "))
-                break
-            except ValueError:
-                print("\nThat isn't a valid option!\n")
-                continue
-        match option:
-            case 1: 
-                print("\nAccounts will be displayed here!\n")
-            case 2:
-                while(True):
-                    try:
-                        age = int(input("How old are you? "))
-                        if age < 0:
-                            continue
-                        break
-                    except ValueError: 
+print("Hello! I am the Wells Fargo Chatbot!\nHow can I help you today?")
+option = 0
+while(option != 5):
+    print("Would you like to...\n1. View Your Accounts\n2. Create An Account\n3. Exit")
+    while(True):
+        try:
+            option = int(input("Choose an option!(Enter a number): "))
+            break
+        except ValueError:
+            print("\nThat isn't a valid option!\n")
+            continue
+    match option:
+        case 1: 
+            print("\nAccounts will be displayed here!\n")
+        case 2:
+            while(True):
+                try:
+                    age = int(input("How old are you? "))
+                    if age < 0:
                         continue
-                if age < 18:
-                    print("Process for a minor")
-                print("Continues with normal process")
-            case 3:
-                print("\nExiting, Goodbye!\n")
-                break
-            case _:
-                print("\nThis isn't a valid option!\n")
+                    break
+                except ValueError: 
+                    continue
+            if age < 18:
+                print("Process for a minor")
+            print("Continues with normal process")
+        case 3:
+            print("\nExiting, Goodbye!\n")
+            break
+        case _:
+            print("\nThis isn't a valid option!\n")
